9f9d417a46042ecfcd7b6a884ccd8131c64ea5ca
puskasba <puskas.balazs.tech2021d@bolyaimovar.com>
2022-10-27 10:34:36 +0200

    ige

--- PycharmProjects/gyak5 2.py
+++ PycharmProjects/gyak5 2.py
@@ -3,15 +3,20 @@ mertekegyseg = input("Add meg a mértékegységet. C = celsius, F = fahrenheit, 
 mibe = input("Mibe szeretné átváltani? C = celsius, F = fahrenheit, K = kelvin ")
 
 if mertekegyseg == "C" and mibe == "F":
-    print ((homerseklet-32)%1.8)
+    print((homerseklet-32)/1.8)
 elif mertekegyseg == "C" and mibe == "K":
     print(homerseklet*1.8+32+273.15)
+elif mertekegyseg == "C" and mibe == "C":
+    print(homerseklet)
 if mertekegyseg == "F" and mibe == "K":
     print(homerseklet+273.15)
 elif mertekegyseg == "F" and mibe == "C":
     print(homerseklet*1.8+32)
+elif mertekegyseg == "F" and mibe == "F":
+    print(homerseklet)
 if mertekegyseg == "K" and mibe == "C":
     print(homerseklet-273.15)
 elif mertekegyseg == "K" and mibe == "F":
     print(1.8*(homerseklet - 273.15) + 32)
-
+elif mertekegyseg == "K" and mibe == "K":
+    print(homerseklet)
